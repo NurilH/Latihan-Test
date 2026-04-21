30115ddca7616f960bf0a35cdd3f58d56a042683
Nuril-Volantis <nuril@volantis.io>
2026-04-21 21:05:46 +0700

    segitiga

---
+++ Segitiga/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	SegitigaSamaKaki(4)
+	SegitigaSikuKiri(5)
+	SegitigaSikuKanan(3)
+}
+
+func SegitigaSamaKaki(count int) {
+	for i := 1; i <= count; i++ {
+		for j := 1; j <= count-i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 1; k <= (2*i - 1); k++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+}
+
+func SegitigaSikuKiri(count int) {
+	for i := 0; i < count; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+}
+
+func SegitigaSikuKanan(count int) {
+	for i := 0; i < count; i++ {
+		for j := 0; j < count-i-1; j++ {
+			fmt.Print(" ")
+		}
+		for k := 0; k <= i; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+}
